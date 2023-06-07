dfa078aa0b525e6f1a17f034f218a91ed12f6a47
hugolnx <hugolnx@gmail.com>
2023-06-07 12:48:51 -0300

    Creates LnxEvent and LnxEventListener abstract classes

---
+++ Runtime/LnxEvent/LnxEventListener.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System;
+
+namespace LnxArch
+{
+    [LnxAutoAdd]
+    public abstract class LnxEventListener<TEvent, TArgs> : MonoBehaviour
+    where TEvent : LnxEvent<TEvent, TArgs>
+    {
+        public EventHandler<TArgs> OnTrigger;
+        private TEvent _event;
+
+        [LnxInit]
+        protected void Init(TEvent lnxEvent)
+        {
+            Debug.Log($"[LnxEventListener] Init {this} with {lnxEvent}");
+            _event = lnxEvent;
+            _event.Subscribe(this);
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log($"[OnEnable] Subscribing {this} to {_event}");
+            _event.Subscribe(this);
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log($"[OnDisable] Unsubscribing {this} from {_event}");
+            _event.Unsubscribe(this);
+        }
+
+        internal void Trigger(object sender, TArgs args)
+        {
+            OnTrigger?.Invoke(sender, args);
+        }
+    }
+}
