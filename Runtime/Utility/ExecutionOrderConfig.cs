d7f313352ad6d446dbf0a8ee68c4c6473e31a371
hugolnx <hugolnx@gmail.com>
2023-06-09 12:38:13 -0300

    Creates LnxReattach that reattaches objects to other points in the hierarchy, but keeps the entity's integrity by linking the reattached object with the entity it belongs

--- Runtime/Utility/ExecutionOrderConfig.cs
+++ Runtime/Utility/ExecutionOrderConfig.cs
@@ -5,5 +5,6 @@ namespace LnxArch
         public const int LnxEntityLink = -9997;
         public const int LnxServiceEntity = -9996;
         public const int LnxEntity = -9995;
+        public const int LnxReattach = 10000;
     }
 }
